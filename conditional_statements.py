cfd0fc201ee9c6d0a2e2a6f8ac0d7bfeab273a3f
JoaoConceicao <119219433+GUICA21@users.noreply.github.com>
2025-10-20 15:12:10 +0100

    Exercices

--- conditional_statements.py
+++ conditional_statements.py
@@ -1,7 +1,24 @@
-x = 5
+x = int(input("Enter a number: "))
 if x < 10:
     print("Smaller.")
 if x > 20:
     print("Bigger.")
+
+print("Done.")
+
+x = 42
+if x > 1:
+    print("More than 1.")
+    if x < 100:
+        print("Less than 100.")
+
+print("Done.")
+
+x = 4
+
+if x > 2:
+    print("Bigger")
+else:
+    print("Smaller")
 
 print("Done.")
