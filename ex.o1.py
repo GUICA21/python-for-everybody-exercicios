807d9926b02f1ac77a3a1dd8f4342ff5469a4e8b
JoaoConceicao <119219433+GUICA21@users.noreply.github.com>
2025-10-20 14:19:29 +0100

    Exercicio 1

--- ex.o1.py
+++ ex.o1.py
@@ -11,3 +11,16 @@ type(ival)
 print(ival + 1)
 
 
+nam = input("Who are you?")
+print("Welcome", nam)
+
+# Convert elevator floors
+inp = input("Europe floor?")
+usf = int(inp) + 1
+print("US floor", usf)
+
+hrs = float(input("Enter Hours:"))
+rate = float(input("Enter Rate:"))
+total = hrs * rate
+print("Pay:", total)
+
