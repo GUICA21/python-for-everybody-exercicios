6d2322b0ad74599cdaeaf59cc7ba89083f7867f0
GUICA21 <joao_conceicao8@hotmail.com>
2025-10-25 15:29:43 +0100

    Exercises

--- functions.py
+++ functions.py
@@ -6,3 +6,17 @@ thing()
 print("Done")
 thing()
 
+
+def greet(lang):
+
+    if lang == "English":
+        print("Hello World")
+    elif lang == "Spanish":
+        print("Hola")
+    else:
+        print("Sorry, I don't understand that")
+
+
+greet("English")
+greet("Spanish")
+greet("Portuguese")
