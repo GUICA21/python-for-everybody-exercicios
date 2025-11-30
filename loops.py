9e371bde58ba743b4090cf4f39f9bc3d97cb91e5
JoaoConceicao <119219433+GUICA21@users.noreply.github.com>
2025-11-30 22:36:01 +0000

    Exercises

--- loops.py
+++ loops.py
@@ -4,3 +4,9 @@ while n > 0:
     n -= 1
 print("Goodbye!")
 print(n)
+
+# Exemplo de Loop For
+friends = ['Joseph', 'Glenn', 'Sally']
+for friend in friends:
+    print('Happy New Year:', friend)
+print('Done!')
